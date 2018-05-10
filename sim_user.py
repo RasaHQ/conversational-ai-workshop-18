e0f6ca394164b257629d58d1de2236c16d85db78
Alan Nichol <alan@golastmile.com>
2018-05-10 17:39:51 +0200

    make sim user stricter

--- sim_user.py
+++ sim_user.py
@@ -28,24 +28,33 @@ class SimulatedUser(object):
         self.preferences = {
             "location": ["paris","london","rome"],
             "people": ["1", "2", "4","6"],
-            "start_date": ["today", "next week", "May 25th"],
-            "end_date": ["tomorrow", "next week", "May 26th"],
+            "startdate": ["today", "next week", "May 25th"],
+            "enddate": ["tomorrow", "next week", "May 26th"],
             "price": ["cheap", "mid-range", "expensive"]
         }
         self.provided = set()
         self.last_utter = None
+        self.last_system_utter = None
 
     def new_state(self):
         self.state = np.random.choice(UserState, p=[0.8,0.05,0.05,0.05,0.05])
 
     def pick_entities(self):
+        # override state if none or all slots have already been provided
+        if len(self.preferences) == len(self.provided):
+            self.state = UserState.CORRECT
+        elif len(self.provided) == 0:
+            self.state = UserState.INFORM
+
         if self.state == UserState.INFORM:
             n_ent = np.random.poisson(self.informativeness - 1) + 1
+            candidates = set(self.preferences.keys()) - self.provided
         elif self.state == UserState.CORRECT:
             n_ent = 1
+            candidates = self.provided
         else:
             return ""
-        ents = np.random.choice(list(self.preferences.keys()),
+        ents = np.random.choice(list(candidates),
                                 size=n_ent,
                                 replace=False)
         #print(ents)
@@ -73,15 +82,24 @@ class SimulatedUser(object):
             return action == "utter_chitchat"
         elif intent in [UserState.INFORM.value, UserState.CORRECT.value]:
             if len(self.provided) < len(self.preferences):
-                return "utter_ask" in action
+                try:
+                    entity = action.split("utter_ask_")[1]
+                    return "utter_ask" in action and entity not in self.provided
+                except:
+                    return False
+            else:
+                return action == "utter_filled_slots"
         elif intent == UserState.DIDTHATWORK.value:
             if len(self.provided) < len(self.preferences):
                 return action == "utter_more_info_hotel"
             else:
                 return action == "utter_worked_hotel"
         elif intent == UserState.EXPLAIN.value:
-            # TODO log previous requested action
-            return "utter_explain" in action
+            try:
+                entity = user.last_system_utter.split("utter_ask_")[1]
+                return action == "utter_explain_{}_hotel".format(entity)
+            except:
+                return "utter_explain" in action
         else:
             raise ValueError("unhandled case: {} {}".format(self.last_utter, action))
 
@@ -90,27 +108,33 @@ def print_story(agent, user_id):
     print(tracker.export_stories())
 
 
-def run_sim_user(agent, max_steps=5):
+def run_sim_user(agent, max_steps=8):
     success = True
     user = SimulatedUser()
     first_utter = "request_hotel"
     result = agent.start_message_handling(first_utter, user.sender_id)
+    user.last_system_utter = result["next_action"]
     agent.continue_message_handling(user.sender_id,
                                     result["next_action"], [])
     for _ in range(max_steps):
         utter = user.next_utter()
-        print("next utter {} ".format(utter))
         result = agent.start_message_handling(utter, user.sender_id)
         action = result["next_action"]
+        if 'utter' in action:
+            user.last_system_utter = action
         if not user.response_correct(action):
             print_story(agent, user.sender_id)
             print(action)
             return 0
         while action is not None:
+            if 'utter' in action:
+                user.last_system_utter = action
             result = agent.continue_message_handling(user.sender_id,
                                                      action,
                                                      [])
             action = result["next_action"]
+
+
     print_story(agent, user.sender_id)
     return 1
 
